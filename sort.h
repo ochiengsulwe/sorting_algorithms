5cdcef103b2c40e8e3421412725706e6e98e3354
ochiengsulwe <kodongochieng@gmail.com>
2023-09-26 09:24:11 -0700

    insertion sort algorithm

--- sort.h
+++ sort.h
@@ -2,7 +2,21 @@
 #define _SORT_H
 
 #include <stdlib.h>
-#include <stdio.hh>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
