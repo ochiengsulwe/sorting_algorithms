907fd623070dce9334e8b48f7a41ac88ae68bf46
ochiengsulwe <kodongochieng@gmail.com>
2023-09-26 09:23:13 -0700

    insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a singly linked list in an ascending order,
+ * 	implementing the insertion sort algorithm.
+ * @list: pointer to the list to be assorted.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while(current)
+	{
+		temp = current->next;
+		while (current->prev && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
+
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
+
+			print_list(*list);
+		}
+		current = temp;
+	}
+}
