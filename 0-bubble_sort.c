01d9d88c3263d5caa16d8071f9e67c8edcbf8914
ochiengsulwe <kodongochieng@gmail.com>
2023-09-26 08:39:40 -0700

    found the bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,14 +22,15 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+
+				for (k = 0; k < size; k++)
+				{
+					printf("%d", array[k]);
+					if (k < size - 1)
+					printf(", ");
+				}
+				printf("\n");
 			}
 		}
 	}
-	for (k = 0; k < size; k++)
-	{
-		printf("%d", array[k]);
-		if (k < size - 1)
-			printf(", ");
-	}
-	printf("\n");
 }
